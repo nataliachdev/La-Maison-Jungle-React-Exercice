6c40cd2f95fe799fd76f1b7e409ba7299448a00d
Natalia Cherman <natalia.cherman@hainaut-ea.be>
2026-09-23 22:08:21 +0200

    Ajout projet react la maison jungle

---
+++ la-maison-jungle/src/components/Cart.js
@@ -0,0 +1,76 @@
+import { useState } from "react";
+import "../styles/Cart.css";
+
+
+function Cart({cart, updateCart}) {
+	const [isOpen, setIsOpen] = useState(false);
+	const total = cart.reduce(
+		(acc, plantType) => acc + plantType.amount * plantType.price, 0
+	)
+
+	//Retirer un élément du panier
+	function removeFromCart(name) {
+		const currentPlant = cart.find((plant) => plant.name === name);
+
+		if (currentPlant.amount > 1) {
+			updateCart(
+				cart.map((plant) =>
+					plant.name === name
+						? { ...plant, amount: plant.amount - 1 }
+						: plant
+				)
+			);
+		} else {
+			updateCart(cart.filter((plant) => plant.name !== name));
+		}
+	}
+
+	return isOpen ? (
+		<div className="lmj-cart">
+			<button className='lmj-cart-toggle-button' onClick={() => setIsOpen(false)}>
+				Fermer
+			</button>
+
+			{cart.length > 0 ? (
+				<div>
+					<h2>Panier</h2>
+					{cart.map(({name, price, amount}, index) => (
+						<div key={`${name}-${index}`}>
+							{name} {price}€ x {amount}
+
+							<button 
+								className="lmj-cart-delete-button"
+								onClick={() => updateCart(cart.filter((plant) => plant.name !== name))}>
+								Supprimer
+							</button>
+
+							<button 
+								className="lmj-cart-delete-button"
+								onClick={() => removeFromCart(name)}>
+								Retirer un élément
+							</button>
+						</div>
+					))}
+
+					<h3>Total : {total}€</h3>
+					<button 
+						className="lmj-cart-delete-button"
+						onClick={() => updateCart([])}>
+						Vider le panier
+					</button>
+				</div>
+			) : (
+				<div>Votre panier est vide.</div>
+			)} 
+		</div>
+
+	) : (
+		<div className='lmj-cart-closed'>
+			<button className='lmj-cart-toggle-button' onClick={() => setIsOpen(true)}>
+				Ouvrir le Panier
+			</button>
+		</div>
+	)
+}
+
+export default Cart;
---
+++ la-maison-jungle/src/components/PlantItem.js
@@ -0,0 +1,23 @@
+import CareScale from './CareScale';
+import '../styles/PlantItem.css';
+
+
+function handleClick(plantName) {
+    alert(`Vous voulez acheter 1 ${plantName} ? Très bon choix`);
+}
+
+function PlantItem( {id, cover, name, light, water, price} ) {
+    return (
+        <li key={id} className='lmj-plant-item' onClick={() => handleClick(name)}>
+            <span className='lmj-plant-item-price'>{price}€</span>
+            <img className='lmj-plant-item-cover' src={cover} alt={`${name} cover`} />
+            {name}
+            <div>
+                <CareScale careType='water' scaleValue={water} />
+                <CareScale careType='light' scaleValue={light} />
+            </div>
+        </li>
+    );
+}
+
+export default PlantItem;
